2c84995707fa8205ac8169aa9303181c183db658
elson <elsonlimengvern@gmail.com>
2022-08-15 12:02:15 -0400

    Added export for links

--- scripts/ResolutionExport.py
+++ scripts/ResolutionExport.py
@@ -23,6 +23,7 @@ with open("scripts/config.json") as jsonFile:
     FULL_LINKED_PATH = configPath["FULL_LINKED_PATH"]
     PROSPECTS_EXPORT_PATH = configPath["PROSPECTS_EXPORT_PATH"]
     PROSPECTS_EXPORT_CSV = configPath["PROSPECTS_EXPORT_CSV"]
+    LINK_EXPORT_CSV = configPath["LINK_EXPORT_CSV"]
 
 linkDF = spark.read.option("header", True).option("inferSchema", True).parquet(FULL_LINKED_PATH)
 tleDF = spark.read.option("header", True).option("inferSchema", True).csv(RAW_TLE_PATH)
@@ -40,3 +41,4 @@ exportDF = (
 exportDF.show()
 exportDF.write.mode("overwrite").parquet(PROSPECTS_EXPORT_PATH)
 exportDF.toPandas().to_csv(PROSPECTS_EXPORT_CSV, index=False)
+linkDF.toPandas().to_csv(LINK_EXPORT_CSV, index=False)
